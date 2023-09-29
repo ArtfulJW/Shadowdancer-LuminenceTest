594dd5da5847e0aa995603f680c7fc6d66929e2f
unknown <jwang550@my.bcit.ca>
2023-09-29 16:52:34 -0700

    Finish

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Numerics;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    private static GameManager _instance;
+    public static GameManager Instance { get { return _instance; } }
+
+    private List<Light> _Lights = new List<Light>();
+
+    // Make sure there is only ever one GameManager
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+
+        // Do stuff
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+    }
+
+    public void addLight(Light inputLight)
+    {
+        _Lights.Add(inputLight);
+
+        Debug.Log("Number of Lights in Level: " + _Lights.Count);
+
+    }
+
+}
--- Assets/Scripts/InstantiateLight.cs
+++ Assets/Scripts/InstantiateLight.cs
@@ -31,15 +31,17 @@ public class InstantiateLight : MonoBehaviour
         lightSphereCollider = light.GetComponent<SphereCollider>();
         lightSphereCollider.radius = minimumRange;
 
-        // Instantiate Light at LightInstatiater_Handler position.
-        Instantiate(light, this.transform.position, Quaternion.identity);
-        // Add light to singletons
+
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Instantiate Light at LightInstatiater_Handler position.
+        Instantiate(light, this.transform.position, Quaternion.identity);
+
+        // Add light to GameManager Singleton
+        GameManager.Instance.addLight(light);
     }
 
     // Update is called once per frame
